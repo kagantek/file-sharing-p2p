165032d8d95461d0a3206c31bb50a7a4c6e0137e
kagantek <kagantek2003@hotmail.com>
2025-01-05 20:05:51 +0300

    FileServer Test

---
+++ src/test/FileServerTest.java
@@ -0,0 +1,33 @@
+package test;
+
+//Kagan Tek - 20210702027 - P2P File Sharing Project - Yeditepe University CSE471 Course
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import p2p.FileDump;
+import p2p.FileServer;
+import p2p.Peer;
+
+public class FileServerTest {
+    public static void main(String[] args) {
+        int port = 6789;
+
+        FileDump dump = new FileDump();
+        try {
+            System.out.println("Generating fileToSend.txt ...");
+            dump.createFile();
+            System.out.println("File generated successfully.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("File generation failed (but continuing).");
+        }
+
+        Peer peer = new Peer(); 
+
+        FileServer server = new FileServer(peer, port);
+
+        server.startServer();
+
+        System.out.println("Server started. Waiting for connections on port " + port + "...");
+    }
+}
