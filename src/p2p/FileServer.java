d9fd33412a3da15bfd25d17289ed8197bc621662
kagantek <kagantek2003@hotmail.com>
2025-01-05 18:24:18 +0300

    runServer method which will be listening at the specified port and creating FileServerThreads accordingly implemented.

--- src/p2p/FileServer.java
+++ src/p2p/FileServer.java
@@ -10,12 +10,14 @@ import java.util.concurrent.*;
 
 //FileServer class will be handling all the connections which will take place in FileServerThread class. 
 public class FileServer {
+    private Peer peer;
     private int port;
     private boolean isRunning;
     private ServerSocket socket;
     private ExecutorService threads; //Will store the FileServerThreads
 
-    public FileServer(int port) {
+    public FileServer(Peer peer, int port) {
+        this.peer = peer;
         this.port = port;
     }
 
@@ -25,11 +27,32 @@ public class FileServer {
         new Thread(() -> runServer()).start();
     }
 
-    public void stopServer() {
-        return;
+    //When stop server is called it will check the socket and existing threads then close them all.
+    public void stopServer() throws IOException {
+        isRunning = false;
+        if(socket != null) socket.close();
+        if(threads != null) threads.shutdownNow();
     }
 
+    //Starting to listen on the specified port and creating threads progressively.
     public void runServer() {
+        try {
+            socket = new ServerSocket();
+            System.out.println("Server listening on port " + port);
+            
+            while(isRunning) {
+                try {
+                    Socket connSocket = socket.accept();
+                    threads.execute(new FileServerThread(peer, connSocket));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Server stopped.");
+        }
         return;
     }
 }
