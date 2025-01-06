66754a7cc8e4adf4c36f88afb582bde551c0016e
kagantek <kagantek2003@hotmail.com>
2025-01-06 19:31:14 +0300

    PeerInfo class implemented

--- src/p2p/PeerInfo.java
+++ src/p2p/PeerInfo.java
@@ -1,5 +1,45 @@
 package p2p;
 
 public class PeerInfo {
-    
+    private String nodeId;
+    private String ip;
+    private int port;
+    private long lastSeen;
+
+    public PeerInfo(String nodeId, String ip, int port) {
+        this.nodeId = nodeId;
+        this.ip = ip;
+        this.port = port;
+        this.lastSeen = System.currentTimeMillis();
+    }
+
+    public String getId() {
+        return nodeId;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public long getLastSeen() {
+        return lastSeen;
+    }
+
+    public void updateLastSeen() {
+        this.lastSeen = System.currentTimeMillis();
+    }
+
+    @Override
+    public String toString() {
+        return "PeerInfo{" +
+                "nodeId='" + nodeId + '\'' +
+                ", address='" + ip + '\'' +
+                ", port=" + port +
+                ", lastSeen=" + lastSeen +
+                '}'; 
+     }
 }
